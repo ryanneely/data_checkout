8a3e3d8a709eb5a8cae1616421b7f237ccc2771f
ryanneely <ryan_m_neely@protonmail.com>
2020-01-03 14:04:09 -0800

    adjusting returned time values to be relative to stim onset/offset and the pad time

--- analysis.py
+++ analysis.py
@@ -10,6 +10,39 @@ import stim_files as sf
 import physio_files as pf
 import bp_files as bf
 
+def get_stim_window(data,start,stop,pad_min=10.0,fix_outliers=[0.25,5]):
+    """
+    function to get data around the time of a stim block.
+    Args:
+        -data: dictionary of data arrays, including the time value. Can have different
+            sample rates as long as they were recorded synchronously. 
+        -start: start time, in ms, of stim block
+        -stop: stop time, in ms, of stim block
+        -pad_min = time, in min, before and after stim to pad the data windows
+        -fix_outliers: if False, does nothing. Otherwise, should be a 2-element interable
+            in which the first value is the max_perc_change variable, and the second element
+            is the sigma variable to use for determining which outliers to remove.
+    Returns:
+        -data: array of data that only includes the data windows requested, and also 
+            has elements corresponding to the start, stop times (in ms) of the stim window.
+    """
+    time = data['time']*1000.0 ##total time of recording, in ms
+    pad = pad_min*60.0*1000.0 ##everything will be in ms for 
+    var = [x for x in list(data) if x !='time']
+    for v in var:
+        y = data[v]
+        if fix_outliers:
+            y = remove_outliers(y,max_perc_change=fix_outliers[0],max_sigma=fix_outliers[1])
+        tbase = np.linspace(0,time,y.size) ##the timebase/sample times for this data array
+        ##replace the data array in the dictionary with just the requested window
+        start_idx = np.where(tbase>start-pad)[0][0]
+        end_idx = np.where(tbase<stop+pad)[0][-1]
+        data[v] = y[start_idx:end_idx]
+    data['start'] = 0
+    data['stop'] = stop-start
+    data['pad'] = pad 
+    return data
+
 def get_stim_info(path):
     """
     A function that looks at a experiment folder path, finds the
@@ -25,8 +58,8 @@ def get_stim_info(path):
             -z: binary stim on/off array spanning the fill recording time
     """
     files = tf.search_files(path) ##dictionary of valid tdms files
-    return sf.process_stim(files['highspeed']) ##assume that the stim mon channel is always in the highspeed data set
-
+    data = sf.process_stim(files['highspeed']) ##assume that the stim mon channel is always in the highspeed data set
+    return data
 
 def get_tstim(start,stop):
     """
@@ -40,7 +73,7 @@ def get_tstim(start,stop):
         -start: single value, in ms, of when as stim block starts
         -stop: single value, in ms, of when a stop block ends
     """
-    return start[0], stop[-1]
+    return start[0]*1000, stop[-1]*1000
 
 def remove_outliers(data,max_perc_change=0.25,max_sigma=5):
     """
