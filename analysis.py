0d93c1ebf17224435c4a3a6231e0748b4c29228f
ryanneely <ryan_m_neely@protonmail.com>
2020-01-03 09:52:00 -0800

    First commit- creating some new fuctions to do deeper analyses beyond just loading/displaying the data

---
+++ analysis.py
@@ -0,0 +1,10 @@
+##analysis.py
+
+##functions to analyze recorded data
+
+##by Ryan Neely 1/3/19
+
+import tdms_files as tf
+import stim_files as sf
+import physio_files as pf
+import bp_files as bf
