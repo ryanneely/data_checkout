546ace3281a332af43221e64433d92ff09adf197
ryanneely <ryan_m_neely@protonmail.com>
2020-01-07 17:29:08 -0800

    Made a nice improvement- using std dev above the mean to set thresholds, because I realized that different rigs scale the stim array differently. Also, I'm sub-sampling the arrays to find mean and std, which is much faster.

--- stim_period.py
+++ stim_period.py
@@ -31,6 +31,7 @@ def get_period(files):
     ##this will churn through the files until a start value is found
     while start == None:
         path = next(files)
+        print("Loading {}".format(path))
         tdms_file = nptdms.TdmsFile(path)
         ##here are a couple diffent possibilities for how things could be named
         try:
@@ -63,14 +64,14 @@ def get_period(files):
         ignore,end = find_stim(channel_object)
     return start,stop
 
-def find_stim(channel_object,thresh=0.1,min_dist=50,min_pulses=30):
+def find_stim(channel_object,sigma=2,min_dist=50,min_pulses=30):
     """
     A function to find the index of a stim onset and offset,
     if it exists, in a tdms channel object.
     Args:
         -channel_object: nptdms channel object containing the stim monitor data
-        -thresh: threshold value of the stim monitor recording to consider the 
-            presence of a stim pulse
+        -sigma: the number of std deviations to use as the threshold for considering a point
+            part of a stim pulse
         -min_dist: the minimum distance (samples) between points to count as an additional pulse.
             At 25kHz, the pulses have a little more than 80pts between them.  
         -min_pulses: the minimum number of pulses needed to count as a real stim train.
@@ -79,7 +80,11 @@ def find_stim(channel_object,thresh=0.1,min_dist=50,min_pulses=30):
     """
     start = None
     stop = None
-    pts = np.where(np.abs(channel_object.data)>thresh)[0] ##any place that the wf crosses the thresh
+    ##let's work with reduced numbers of points to reduce the memory overhead
+    mean = np.mean(channel_object.data[::10]) ##get the mean so we can remove any DC offset
+    std = np.std(channel_object.data[::10])
+    sigma = sigma*std ##t
+    pts = np.where(np.abs(channel_object.data-mean)>sigma)[0] ##any place that the wf crosses the thresh
     dist = np.diff(pts) ##the distance between points exceeding the threshold
     n_pulses = np.where(dist>min_dist)[0] #these should be the points of all the separate pulses
     if n_pulses.size>min_pulses:
